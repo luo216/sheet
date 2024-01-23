d06b166eb4afa1dc70c10e41b5f42711a4f76510
steve <3337990267@qq.com>
2024-01-23 23:17:00 +0800

    finish python to javascript

--- static/js/example.js
+++ static/js/example.js
@@ -7,19 +7,47 @@ function chart_pie(data){
       }
     ]
   }
-  for (var i = 0; i < data.data.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     option.series[0].data.push({
-      name: data.data[i][0],
-      value: data.data[i][1]
+      name: data[i][0],
+      value: data[i][1]
     })
   }
  
   return option
 } 
 
+function chart_bar(data) {
+  var option = {
+      xAxis: {
+        type: 'category',
+        data: []
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [],
+          type: 'bar'
+        }
+      ]
+  }
+
+  for (var i = 0; i < data.length; i++) {
+    option.xAxis.data.push(data[i][0])
+    option.series[0].data.push(data[i][1])
+  }
+
+  return option
+}
+
 function get_option(data) {
   if (data.chart_type=='pie'){
-    return chart_pie(data)
+    return chart_pie(data.data)
+  }
+  if (data.chart_type=='bar'){
+    return chart_bar(data.data)
   }
 
   return {}
